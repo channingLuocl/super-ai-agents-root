7d608cde7890cf2b8cd25b54fc60040d827dbdac
luocl <2301155695@qq.com>
2026-04-03 21:19:14 +0800

    frontend:修复字体图标问题

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import { createHead } from '@vueuse/head'
 import './style.css'
+import '@fontsource/material-symbols-outlined'
 
 const app = createApp(App)
 const head = createHead()
